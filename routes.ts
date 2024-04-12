dccf2d661d133fab03dab8be7b7ae7a61c12a692
eliasmartins <rhyanlucaseliasmartins@gmail.com>
2024-04-12 13:54:35 +0000

    routes

--- routes.ts
+++ routes.ts
@@ -4,7 +4,7 @@
  * @type {string[]}
  */
 
-export const publicRoutes: string[] = ["/", "/api/uploadthing"];
+export const publicRoutes: string[] = ["/api/uploadthing"];
 
 /**
  * An array of routes that are used for authentication
